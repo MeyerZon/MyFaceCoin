1a60a365fcc79eee3a8aa9920b0ffa7a121c948b
MeyerZon <yonathan.meyerzon@gmail.com>
2024-05-30 16:50:34 +0300

    done

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,8 +1,7 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     {link:"#", content:"My Face", icon:"lucide:home"},
+     {link:"#about", content:"My Face Again", icon:"octicon:person-24"},
+
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
